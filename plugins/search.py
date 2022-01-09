63df98f743596b3f82637c75f4de7768e09b395e
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-10 01:09:12 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -12,34 +12,74 @@ from youtubesearchpython import VideosSearch
 
 
 
-@Client.on_message(filters.private & filters.command(["starrt"]))
-async def starrt(update,message):
-	await message.reply_text("Helo iam Youtube Video Search\nUse in inline mode")
-	
+import os
+import ytthumb
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultPhoto
+from youtubesearchpython import VideosSearch
 
 
-@Client.on_message(filters.private & filters.command(["search"]))
-async def search_video(client,message):
-	search = []
-	result = Search(update.message.strip()).videos()
-	for i in result:
-		try:
-			title = i["title"]
-			id = i["id"]
-			thumb = i["thumb"][0]
-			data = i["simple_data"]
-		except:
-			pass
-		try:
-			search.append(
+
+
+
+@Bot.on_message(filters.private & filters.all)
+async def text(bot, update):
+    
+    text = "Search youtube videos using below buttons.\n\nMade by @FayasNoushad"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text="Search here", switch_inline_query_current_chat="")],
+            [InlineKeyboardButton(text="Search in another chat", switch_inline_query="")]
+        ]
+    )
+    
+    await update.reply_text(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Client.on_message(filters.command("search"))
+
+async def search(bot, update):
+    
+    results = VideosSearch(update.answer, limit=50).result()
+    answers = []
+    
+    for result in results:
+        title = result["title"]
+        views_short = result["viewCount"]["short"]
+        duration = result["duration"]
+        duration_text = result["accessibility"]["duration"]
+        views = result["viewCount"]["text"]
+        publishedtime = result["publishedTime"]
+        channel_name = result["channel"]["name"]
+        channel_link = result["channel"]["link"]
+        description = f"{views_short} | {duration}"
+        details = f"**Title:** {title}" + "\n" \
+        f"**Channel:** [{channel_name}]({channel_link})" + "\n" \
+        f"**Duration:** {duration_text}" + "\n" \
+        f"**Views:** {views}" + "\n" \
+        f"**Published Time:** {publishedtime}" + "\n" \
+        "\n" + "**Made by @FayasNoushad**"
+        thumbnail = ytthumb.thumbnail(result["id"])
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Watch Video 📹", url=result["link"])]]
+        )
+        try:
+            answers.append(
                 InlineQueryResultPhoto(
                     title=title,
-                    description=data,
-                    caption="https://youtu.be/"+id,
-                    photo_url=thumb))
-		
-		except:
-		          pass
-            
-	await message.answer(search)
-	
+                    description=description,
+                    caption=details,
+                    photo_url=thumbnail,
+                    reply_markup=reply_markup
+                )
+            )
+        except:
+            pass
+    
+    await update.answer(answers)
