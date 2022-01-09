2615b99ec9bd8828eabec1d2a8915026dcb4e628
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-09 23:21:27 +0530

    Update auto_caption.py

--- plugins/auto_caption.py
+++ plugins/auto_caption.py
@@ -12,18 +12,7 @@ import logging
 import pyrogram
 
 
-if __name__ == "__main__" :
-    plugins = dict(
-        root="mt_privateautocaption"
-    )
-    Jebot= pyrogram.Client(
-        "CaptionBot",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins,
-        workers=300
-    )
+
     
 
 
