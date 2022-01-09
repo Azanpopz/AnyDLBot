a1503341f0d24639ca4888f879df9b7a3f2e8849
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-10 01:55:09 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -38,24 +38,42 @@ async def text(bot, update):
     
 
 @Client.on_message(filters.command("search"))
-
 async def search(bot, update):
-    await message.reply(
-    results = VideosSearch(update.message, limit=5).result()
+    
+    results = VideosSearch(update.search, limit=50).result()
     answers = []
     
     for result in results:
+        title = result["title"]
+        views_short = result["viewCount"]["short"]
+        duration = result["duration"]
+        duration_text = result["accessibility"]["duration"]
+        views = result["viewCount"]["text"]
+        publishedtime = result["publishedTime"]
+        channel_name = result["channel"]["name"]
+        channel_link = result["channel"]["link"]
+        description = f"{views_short} | {duration}"
+        details = f"**Title:** {title}" + "\n" \
+        f"**Channel:** [{channel_name}]({channel_link})" + "\n" \
+        f"**Duration:** {duration_text}" + "\n" \
+        f"**Views:** {views}" + "\n" \
+        f"**Published Time:** {publishedtime}" + "\n" \
+        "\n" + "**Made by @FayasNoushad**"
         thumbnail = ytthumb.thumbnail(result["id"])
         reply_markup = InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="Watch Video 📹", url=result["link"])]]
         )
-        
+        try:
+            answers.append(
+                InlineQueryResultPhoto(
+                    title=title,
+                    description=description,
+                    caption=details,
+                    photo_url=thumbnail,
+                    reply_markup=reply_markup
+                )
+            )
+        except:
+            pass
     
-    
-    await update.reply(
-        text=results,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        quote=True
-    )
-
+    await update.answer(answers)
