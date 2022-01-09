27ea9d080cec3b0e67018fca1f90001f83d2a946
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-09 23:51:50 +0530

    Create thumb.py

---
+++ plugins/thumb.py
@@ -0,0 +1,36 @@
+import os
+import ytthumb
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import Config
+
+@Client.on_message(filters.private & filters.command(["ytthumb"]))
+async def send_thumbnail(bot, update):
+    message = await update.reply_text(
+        text="`Analysing...`",
+        disable_web_page_preview=True,
+        quote=True
+    )
+    try:
+        if " | " in update.text:
+            video = update.text.split(" | ", -1)[0]
+            quality = update.text.split(" | ", -1)[1]
+        else:
+            video = update.text
+            quality = "sd"
+        thumbnail = ytthumb.thumbnail(
+            video=video,
+            quality=quality
+        )
+        await update.reply_photo(
+            photo=thumbnail,
+            reply_markup=BUTTONS,
+            quote=True
+        )
+        await message.delete()
+    except Exception as error:
+        await message.edit_text(
+            text=error,
+            disable_web_page_preview=True,
+            reply_markup=BUTTONS
+        )
