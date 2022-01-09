ec856daa0c069c2e61974fada86a1ca860f1c530
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-10 01:41:19 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -45,21 +45,6 @@ async def search(bot, update):
     answers = []
     
     for result in results:
-        title = result["title"]
-        views_short = result["viewCount"]["short"]
-        duration = result["duration"]
-        duration_text = result["accessibility"]["duration"]
-        views = result["viewCount"]["text"]
-        publishedtime = result["publishedTime"]
-        channel_name = result["channel"]["name"]
-        channel_link = result["channel"]["link"]
-        description = f"{views_short} | {duration}"
-        details = f"**Title:** {title}" + "\n" \
-        f"**Channel:** [{channel_name}]({channel_link})" + "\n" \
-        f"**Duration:** {duration_text}" + "\n" \
-        f"**Views:** {views}" + "\n" \
-        f"**Published Time:** {publishedtime}" + "\n" \
-        "\n" + "**Made by @FayasNoushad**"
         thumbnail = ytthumb.thumbnail(result["id"])
         reply_markup = InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="Watch Video 📹", url=result["link"])]]
