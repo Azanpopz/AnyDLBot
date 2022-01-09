67acb86464e2a3a49ff89e4c16148d9071e80a4d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-09 23:14:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ from opencc import OpenCC
 from config import Config
 import wget
 
-Jebot = Client(
-   "AnyDL Bot",
-   api_id=Config.API_ID,
-   api_hash=Config.API_HASH,
-   bot_token=Config.BOT_TOKEN,
-)
+import os
+import logging
+import pyrogram
+from config import Config  
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="mt_privateautocaption"
+    )
+    Jebot= pyrogram.Client(
+        "CaptionBot",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins,
+        workers=300
+    )
+    
 
 YTDL_REGEX = (r"^((?:https?:)?\/\/)"
               r"?((?:www|m)\.)"
