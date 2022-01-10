d80a551c73e5d259bc3f62af2c49f2d466aa8035
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-10 18:08:10 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -58,7 +58,7 @@ async def ytsearch(_, message: Message):
             text += f"Channel - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             reply_markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Watch Video 📹", url=f"https://youtube.com{results[i]['url_suffix'])]]
+            [[InlineKeyboardButton(text='Watch Video 📹', url=f"https://youtube.com{results[i]['url_suffix'])}]]
         )
             i += 1
         await m.edit(text, disable_web_page_preview=True)
